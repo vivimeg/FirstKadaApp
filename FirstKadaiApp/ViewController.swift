209525f2398f675041d19bf3026fed957b9e584b
megumi.K <daron.d.libra@gmail.com>
2020-12-28 21:27:58 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 片桐めぐみ on 2020/12/28.
+//  Copyright © 2020 megumi.katagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
